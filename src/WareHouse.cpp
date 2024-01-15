269c576435fa6a036c2126a61577805ec54ad3ac
dov <1dovko@gmail.com>
2024-01-15 20:17:11 +0200

    updating warehose.cpp

---
+++ src/WareHouse.cpp
@@ -0,0 +1,36 @@
+#include "../include/WareHouse.h"
+#include <fstream>
+
+class WareHouse {
+
+    public:
+        WareHouse(const string &configFilePath){
+            ifstream configFile {configFilePath};
+            string line {};
+            while (getline(configFile, line)){
+
+            }
+        }
+        void start();
+        const vector<BaseAction*> &getActionsLog() const;
+        void addOrder(Order* order);
+        void addAction(BaseAction* action);
+        void printActionsLogs();
+        Customer &getCustomer(int customerId) const;
+        Volunteer &getVolunteer(int volunteerId) const;
+        Order &getOrder(int orderId) const;
+        void close();
+        void open();
+
+    private:
+        bool isOpen;
+        vector<BaseAction*> actionsLog;
+        vector<Volunteer*> volunteers;
+        vector<Order*> pendingOrders;
+        vector<Order*> vol;
+        vector<Order*> completedOrders;
+        vector<Customer*> customers;
+        int customerCounter; //For assigning unique customer IDs
+        int volunteerCounter; //For assigning unique volunteer IDs
+};
+
